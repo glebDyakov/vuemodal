5e8268e6199f849ecee07398b614cd7193522124
Rodion <diakovrodion@mail.ru>
2021-02-14 10:23:34 +0300

    init

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="alert" :class="type">
+        <p class="alert-title">{{title}}</p>
+        <p>{{text}}</p>
+        <button class="btn" :class="type" v-if="closable" @click="$emit('close')">{{title}}</button>
+    </div>
+</template>
+<script>
+export default {
+    emits:['close'],
+    props:{
+        text:String,
+        title:String,
+        closable: {
+            type:Boolean,
+            required: false,
+            default:false
+        },
+        type: {
+            type:String,
+            required: false,
+            default:'primary',
+            validator(val){
+                return ['primary', 'danger', 'warning'].includes(val)
+            }
+        },
+        
+    }
+}
+</script>
